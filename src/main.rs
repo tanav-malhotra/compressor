de8c5040f974eacb46124eef69e465ba7fda5548
Tanav Malhotra <161088369+tanav-malhotra@users.noreply.github.com>
2024-04-10 13:27:19 -0400

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env;
+use std::fs::File;
+use std::io::{copy, BufReader};
+use std::time::Instant;
+use std::process;
+
+fn main() {
+    if env::args().len() != 3 {
+        println!("Usage: compressor `source` `target`");
+        process::exit(0);        
+    }
+
+    let mut input = BufReader::new(File::open(env::args().nth(1).unwrap()).unwrap());
+    let output = File::create(env::args().nth(2).unwrap()).unwrap();
+
+    let mut encoder = GzEncoder::new(output, Compression::default());
+
+    let start = Instant::now();
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+    let elapsed_time = start.elapsed();
+    println!(
+        "Source size: {:?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+    println!("Target size: {:?}", output.metadata().unwrap().len());
+    println!("Elapsed time: {:?}", elapsed_time);
+}
